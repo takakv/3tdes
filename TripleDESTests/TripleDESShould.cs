0d0d247fad5e12676a74dd4c1d99fca7b8befb76
Taka <71265675+takakv@users.noreply.github.com>
2021-04-01 02:54:48 +0300

    Refactor test bool arrays

--- TripleDESTests/TripleDESShould.cs
+++ TripleDESTests/TripleDESShould.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using FluentAssertions;
 using TripleDES;
 using Xunit;
@@ -12,30 +11,31 @@ namespace TripleDESTests
 {
     public class TripleDESShould
     {
+        private const bool F = false;
+        private const bool T = true;
+
         [Fact]
         public void GetKeyBits()
         {
-            const string key = "I am groot";
-            string[] keyBitStrings =
+            const string key = "key bits";
+
+            bool[] bitBoolsCheck =
             {
-                "01001001", "00100000", "01100001", "01101101", "00100000", "01100111", "01110010",
-                "01101111", "01101111", "01110100"
+                F, T, T, F, T, F, T, T,
+                F, T, T, F, F, T, F, T,
+                F, T, T, T, T, F, F, T,
+                F, F, T, F, F, F, F, F,
+                F, T, T, F, F, F, T, F,
+                F, T, T, F, T, F, F, T,
+                F, T, T, T, F, T, F, F,
+                F, T, T, T, F, F, T, T
             };
 
-            List<bool> tmp = new List<bool>();
+            BitArray bits = DES.GetBitsFromString(key);
+            bool[] bitBools = new bool[bits.Count];
+            bits.CopyTo(bitBools, 0);
 
-            // The least significant bit represents the lowest index value.
-            // On bits are represented by truth values.
-            foreach (string bitString in keyBitStrings)
-                for (int j = 7; j >= 0; --j)
-                    tmp.Add(bitString[j] == '1');
-            bool[] keyBitBoolsCheck = tmp.ToArray();
-
-            BitArray keyBits = DES.GetBitsFromString(key);
-            bool[] keyBitBools = new bool[keyBits.Count];
-            keyBits.CopyTo(keyBitBools, 0);
-
-            keyBitBools.Should().BeEquivalentTo(keyBitBoolsCheck);
+            bitBools.Should().BeEquivalentTo(bitBoolsCheck);
         }
 
         [Fact]
@@ -45,11 +45,14 @@ namespace TripleDESTests
 
             bool[] bitBoolsCheck =
             {
-                true, false, true, false, false, true, true, true, true, false, false, true, false,
-                false, false, true, false, true, false, false, false, false, true, false, false,
-                false, true, false, true, true, true, true, false, true, true, true, true, true,
-                true, true, true, true, true, true, true, true, false, false, false, true, false,
-                false, false, true, false, true
+                T, F, T, F, T, F, T,
+                T, T, F, F, T, F, F,
+                F, T, F, T, F, F, F,
+                F, T, F, F, F, T, F,
+                T, T, T, T, F, T, T,
+                T, T, T, T, T, T, T,
+                T, T, T, T, F, F, F,
+                T, F, F, F, T, F, T
             };
 
             BitArray bits = DES.GetPermutedKey(new BitArray(bytes));
