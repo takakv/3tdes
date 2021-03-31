c807b4c47c8c8a4b39e1d6c81fdd776220a7ec64
Taka <71265675+takakv@users.noreply.github.com>
2021-03-31 21:37:26 +0300

    Refactor permutation into its own function

---
+++ README.md
@@ -0,0 +1,5 @@
+# 3-key Triple DES
+
+A from the ground up implementation of 3-key Triple DES I wrote in C# to familiarise myself with the standard. It is not a "by the letter" implementation, as I brushed over some parts such as checking the parity bits, which I did not find critical for understanding the algorithm itself.
+
+[NIST DES specification (archived)](https://csrc.nist.gov/CSRC/media/Publications/fips/46/3/archive/1999-10-25/documents/fips46-3.pdf)
--- TDES/Program.cs
+++ TDES/Program.cs
@@ -12,21 +12,11 @@ namespace TDES
 
         private static void Main(string[] args)
         {
-            byte[] keyBytes = GetKey();
-            var keyBits = new BitArray(keyBytes);
+            BitArray permutedKeyBits = GetPermutedKey(GetKey());
 
-            // Key permutation table:
-            // https://csrc.nist.gov/CSRC/media/Publications/fips/46/3/archive/1999-10-25/documents/fips46-3.pdf#page=24
-            int[] keyPermutationsList =
-            {
-                57, 49, 41, 33, 25, 17, 9, 1, 58, 50, 42, 34, 26, 18, 10, 2, 59, 51, 43, 35, 27, 19,
-                11, 3, 60, 52, 44, 36, 63, 55, 47, 39, 31, 23, 15, 7, 62, 54, 46, 38, 30, 22, 14, 6,
-                61, 53, 45, 37, 29, 21, 13, 5, 28, 20, 12, 4
-            };
-
-            // There is a parity check bit for each byte, which gets ignored.
-            var permutedKeyBits = new BitArray(56);
-            for (var i = 0; i < 56; ++i) permutedKeyBits[i] = keyBits[keyPermutationsList[i] - 1];
+            // Get the 16 subkeys for each of the DES rounds.
+            var subKeys = new BitArray[16];
+            for (var i = 0; i < 16; ++i) subKeys[i] = GetSubKey(permutedKeyBits, i);
 
             string plaintext = GetPlaintext();
             byte[] inputBytes = GetPlaintextBytes(plaintext);
@@ -39,13 +29,39 @@ namespace TDES
                 inputBlocks[i, j] = inputBytes[j + i * BlockSize];
         }
 
-        // ReSharper disable once ReturnTypeCanBeEnumerable.Local
-        private static byte[] GetKey()
+        private static BitArray GetPermutedKey(BitArray bits)
+        {
+            // Key permutation table:
+            // https://csrc.nist.gov/CSRC/media/Publications/fips/46/3/archive/1999-10-25/documents/fips46-3.pdf#page=24
+            int[] permutations =
+            {
+                57, 49, 41, 33, 25, 17, 9, 1, 58, 50, 42, 34, 26, 18, 10, 2, 59, 51, 43, 35, 27, 19,
+                11, 3, 60, 52, 44, 36, 63, 55, 47, 39, 31, 23, 15, 7, 62, 54, 46, 38, 30, 22, 14, 6,
+                61, 53, 45, 37, 29, 21, 13, 5, 28, 20, 12, 4
+            };
+
+            // There is a parity check bit for each byte, which gets ignored.
+            var permutedBits = new BitArray(56);
+            for (var i = 0; i < 56; ++i) permutedBits[i] = bits[permutations[i] - 1];
+            return permutedBits;
+        }
+
+        private static BitArray GetSubKey(BitArray key, int iteration)
+        {
+            var keyBitsL = new BitArray(28);
+            var keyBitsR = new BitArray(28);
+            for (var i = 0; i < 28; ++i) keyBitsL[i] = key[i];
+            for (var i = 0; i < 28; ++i) keyBitsR[i] = key[28 + i];
+            var subKey = new BitArray(56);
+            return subKey;
+        }
+
+        private static BitArray GetKey()
         {
             Console.WriteLine($"Enter your key ({BlockSize} ASCII characters long):");
             string key = Console.ReadLine();
             if (!string.IsNullOrEmpty(key) && key.Length == BlockSize)
-                return Encoding.ASCII.GetBytes(key);
+                return new BitArray(Encoding.ASCII.GetBytes(key));
 
             Console.WriteLine("You have not provided valid input. Exiting the program...");
             Environment.Exit(0);
