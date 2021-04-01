deb662e36d96771e8d84cf44592260a88dd441b9
Taka <71265675+takakv@users.noreply.github.com>
2021-04-01 03:24:39 +0300

    Add bit order warning to tests

--- TripleDESTests/TripleDESShould.cs
+++ TripleDESTests/TripleDESShould.cs
@@ -7,6 +7,15 @@ using Xunit;
 // ReSharper disable SuggestVarOrType_Elsewhere
 // ReSharper disable SuggestVarOrType_BuiltInTypes
 
+// ASCII: key bits
+// Bit string: 01101011 01100101 01111001 00100000 01100010 01101001 01110100 01110011
+// Index-form bit string: 11010110 10100110 10011110 00000100 01000110 10010110 00101110 11001110
+// Decimal: 107 101 121 32 98 105 116 115
+
+// NOTE: The Should().BeEquivalentTo() method does not care if the comparing bit array
+// uses bit string order or index-form order. However, I have provided all bit check arrays
+// in the index-form, as that's how BitArray represents the bits internally.
+
 namespace TripleDESTests
 {
     public class TripleDESShould
@@ -21,14 +30,14 @@ namespace TripleDESTests
 
             bool[] bitBoolsCheck =
             {
-                F, T, T, F, T, F, T, T,
-                F, T, T, F, F, T, F, T,
-                F, T, T, T, T, F, F, T,
-                F, F, T, F, F, F, F, F,
-                F, T, T, F, F, F, T, F,
-                F, T, T, F, T, F, F, T,
-                F, T, T, T, F, T, F, F,
-                F, T, T, T, F, F, T, T
+                T, T, F, T, F, T, T, F,
+                T, F, T, F, F, T, T, F,
+                T, F, F, T, T, T, T, F,
+                F, F, F, F, F, T, F, F,
+                F, T, F, F, F, T, T, F,
+                T, F, F, T, F, T, T, F,
+                F, F, T, F, T, T, T, F,
+                T, T, F, F, T, T, T, F
             };
 
             BitArray bits = DES.GetBitsFromString(key);
